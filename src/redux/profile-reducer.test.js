ee13d0cc38f4506d2c674faa7f8e1a74c55c81a6
unknown <cool.gilmitdinov@mail.com>
2022-04-04 19:15:33 +0300

    created profile-reducer.test

---
+++ src/redux/profile-reducer.test.js
@@ -0,0 +1,35 @@
+import { addPostActionCreator, profileReducer } from "./profile-reducer";
+
+test("post length should be 5", () => {
+  let state = {
+    posts: [
+      { id: 1, message: "hello" },
+      { id: 2, message: "hello" },
+      { id: 3, message: "hello" },
+      { id: 4, message: "hello" },
+      { id: 5, message: "hello" },
+    ],
+  };
+  let action = addPostActionCreator("hello");
+
+  let newState = profileReducer(state, action)
+
+  expect(newState.posts.length).toBe(6);
+});
+
+test("post message should be correct", () => {
+  let state = {
+    posts: [
+      { id: 1, message: "hello" },
+      { id: 2, message: "hello" },
+      { id: 3, message: "hello" },
+      { id: 4, message: "hello" },
+      { id: 5, message: "hello" },
+    ],
+  };
+  let action = addPostActionCreator("hello");
+
+  let newState = profileReducer(state, action)
+
+  expect(newState.posts[5].message).toBe("hello");
+});
